8221e44f8f7f288ccebb3bacdc435bc5f9a80af4
Mouad <elhilali.mad@gmail.com>
2023-02-16 15:41:01 +0100

    Configuration workspace (je dois modifier le readme)

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,13 +1,6 @@
-// import {windowContext} from '../../client/src/renderer'
 import { MainRenderer } from './renderer'
 
 const root = document.getElementById('root') as HTMLElement
-
-
-
 document.addEventListener('DOMContentLoaded', () => {
   MainRenderer(root , null);
 })
-
-// MainRenderer(root , null)
-
