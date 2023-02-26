ffcfd6dd021c7bdcea3a9237945cfe27df3978e6
Mouad <elhilali.mad@gmail.com>
2023-02-26 14:56:40 +0100

    Hotfix : make client load assets

--- apps/client/webpack.renderer.config.ts
+++ apps/client/webpack.renderer.config.ts
@@ -1,19 +1,28 @@
-import type { Configuration } from 'webpack';
+import type { Configuration } from "webpack";
 
-import { rules } from './webpack.rules';
-import { plugins } from './webpack.plugins';
+import { rules } from "./webpack.rules";
+import { plugins } from "./webpack.plugins";
 
-rules.push({
-  test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
-});
+rules.push(
+  {
+    test: /\.css$/,
+    use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+  },
+  {
+    test: /\.png/,
+    type: 'asset/resource'
+  }
+);
 
 export const rendererConfig: Configuration = {
   module: {
     rules,
   },
+  output: {
+    assetModuleFilename: 'assets/[name][ext]',
+  },
   plugins,
   resolve: {
-    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
+    extensions: [".js", ".ts", ".jsx", ".tsx", ".css"],
   },
 };
