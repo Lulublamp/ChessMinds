560e8258248d77056f1b03f20259e099758b81fd
Mouad <elhilali.mad@gmail.com>
2023-02-26 15:05:49 +0100

    Hotfix: Remplacer les KebabCase par des CamelCase (React comprend pas les props avec des `-`)

--- apps/frontend/src/pages/components/Navbar.tsx
+++ apps/frontend/src/pages/components/Navbar.tsx
@@ -15,8 +15,8 @@ function Navbar() {
       </div>
       <div>
         <svg width="22" height="25" viewBox="0 0 22 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M17.875 23.5001H4.125C2.39911 23.5001 1 22.101 1 20.3751C1 15.2742 8.5 15.3751 11 15.3751C13.5 15.3751 21 15.2742 21 20.3751C21 22.101 19.6009 23.5001 17.875 23.5001Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-          <path d="M11 11C13.7614 11 16 8.76142 16 6C16 3.23858 13.7614 1 11 1C8.23858 1 6 3.23858 6 6C6 8.76142 8.23858 11 11 11Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M17.875 23.5001H4.125C2.39911 23.5001 1 22.101 1 20.3751C1 15.2742 8.5 15.3751 11 15.3751C13.5 15.3751 21 15.2742 21 20.3751C21 22.101 19.6009 23.5001 17.875 23.5001Z" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+          <path d="M11 11C13.7614 11 16 8.76142 16 6C16 3.23858 13.7614 1 11 1C8.23858 1 6 3.23858 6 6C6 8.76142 8.23858 11 11 11Z" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
         </svg>
         <a>Join Us</a>                
       </div>
