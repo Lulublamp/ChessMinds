45bfae30d547332bc5cce28a94e5d286a2850ef1
JAIDANE CHAIMA <cjaidane@etu.unistra.fr>
2023-03-02 00:10:39 +0100

    le hashage

--- apps/backend/src/joueurs/joueurs.service.ts
+++ apps/backend/src/joueurs/joueurs.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { create } from 'domain';
+import { hashPassword } from 'src/utils/bcrypt';
 import { Repository } from 'typeorm';
 import { JoueurDto } from './DTO/joueurs.dto';
 import { Joueur } from './entities/joueur.entity';
@@ -15,10 +16,18 @@ export class JoueursService {
   //retrouver un joueur avec le Repository
   async findJoueur(idJoueur: number) {
     return await this.joueursRepository.find();
+    //id du joueur fait une erreur je sais pas pk à voir ca
   }
   //enregistrer un joueur avec le Repository
   inscriptionJoueur(joueur: JoueurDto) {
-    const joueurInscrit = this.joueursRepository.create(joueur);
+    //hashage du mot de passe
+    const motDePasse= hashPassword(joueur.motDePasse);
+    const joueurInscrit = this.joueursRepository.create({...joueur,motDePasse});
     return this.joueursRepository.save(joueurInscrit);
+  }
+  //modifier un joueur avec le Repository
+  updateJoueur(idJoueur:number, joueur: JoueurDto) {
+    const joueurUpdate = this.joueursRepository.update(idJoueur, joueur);
+    return joueurUpdate;
   }
 }
