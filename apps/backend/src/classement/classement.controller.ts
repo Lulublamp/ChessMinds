e9f691a18187e413a7e0825e3b3abd718122f4dd
JAIDANE CHAIMA <cjaidane@etu.unistra.fr>
2023-03-01 23:28:42 +0100

    ajout controller part 2

---
+++ apps/backend/src/classement/classement.controller.ts
@@ -0,0 +1,31 @@
+import { Body, Controller, Get, Param, ParseIntPipe, Put } from '@nestjs/common';
+import { ClassementService } from './classement.service';
+import { ClassementDto } from './DTO/classement.dto';
+
+@Controller('classement')
+export class ClassementController {
+  constructor(
+    private readonly classementService: ClassementService) {}
+
+//si on veut recupere classement
+  @Get()
+  async retourneClassement(@Body() classement: ClassementDto) {
+  //  const classements= await this.classementService.findClassement();
+    //return classements;
+  }
+
+//si on veut modifier le classement
+ /* @Put('update')
+  updateClassement(
+    @Param('idJoueur', ParseIntPipe) idJoeur: number,
+    @Body() classement: ClassementDto
+  ) {
+    try {
+      const classementUpdate = this.classementService.updateClassement(idJoeur,classement);
+      return classementUpdate;
+    }catch (error){
+      console.log(error)
+      return error;
+    }
+  }*/
+}
