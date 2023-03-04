aac86a2715b24a5bd3859907778063d9e8d597f7
JAIDANE CHAIMA <cjaidane@etu.unistra.fr>
2023-03-04 11:49:00 +0100

    ajout le fait que email est unique dans DTO

--- apps/backend/src/joueurs/DTO/joueurs.dto.ts
+++ apps/backend/src/joueurs/DTO/joueurs.dto.ts
@@ -1,7 +1,11 @@
 /* eslint-disable prettier/prettier */
 import { IsEmail, IsNotEmpty, IsString, Matches } from 'class-validator';
+import { Unique } from 'typeorm';
+
+
 
 export class JoueurDto {
+  @Unique(['adresseMail'])
   @IsNotEmpty()
   @IsEmail() // pour dire que ce qui entre en email doit etre sous forme d'email
   adresseMail: string;
