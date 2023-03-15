98138501981a35d99e876ad3f555822b662dffef
JAIDANE CHAIMA <cjaidane@etu.unistra.fr>
2023-03-15 19:08:00 +0100

    supp des classements diff niveau (quiproquo)

--- apps/backend/src/classement_blitz/classement_blitz.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ClassementBlitzService } from './classement_blitz.service';
-import { ClassementBlitzController } from './classement_blitz.controller';
-
-@Module({
-  providers: [ClassementBlitzService],
-  controllers: [ClassementBlitzController]
-})
-export class ClassementBlitzModule {}
--- apps/backend/src/classement_bullet/classement_bullet.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ClassementBulletService } from './classement_bullet.service';
-
-@Module({
-  providers: [ClassementBulletService]
-})
-export class ClassementBulletModule {
-  
-}
