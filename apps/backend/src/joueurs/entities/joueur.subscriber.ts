a107a58a59c013fee0e89bcbe62e582b60b21ef1
Mouad <elhilali.mad@gmail.com>
2023-03-25 19:20:04 +0100

    Fix subscriber error

--- apps/backend/src/joueurs/entities/joueur.subscriber.ts
+++ apps/backend/src/joueurs/entities/joueur.subscriber.ts
@@ -1,30 +1,56 @@
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { EntitySubscriberInterface, EventSubscriber, InsertEvent, Repository} from 'typeorm';
+import { PlayerNotFound } from 'src/errors/bErrors';
+import { Connection, EntitySubscriberInterface, EventSubscriber, InsertEvent, Repository} from 'typeorm';
+import { JoueursService } from '../joueurs.service';
 import { Joueur } from './joueur.entity';
 
 @Injectable()
 @EventSubscriber()
 export class JoueurSubscriber implements EntitySubscriberInterface<Joueur> {
 
-  constructor(@InjectRepository(Joueur) private readonly joueurRepository: Repository<Joueur>) {
+  constructor(private readonly joueurService: JoueursService,
+              private connection: Connection) {
+    connection.subscribers.push(this);
   }
 
   listenTo() {
     return Joueur;
   }
 
-  async beforeInsert(event: InsertEvent<Joueur>) {
-    const sameName = await this.joueurRepository.findOneBy({ pseudo: event.entity.pseudo });
-    let randomNumber = Math.floor(Math.random() * 9000) + 1000;
-    if (sameName) {
-      while (sameName.tagJoueur === `${randomNumber}`) {
-        randomNumber = Math.floor(Math.random() * 9000) + 1000;
-      }
-    }
+  async addTag(event: InsertEvent<Joueur>) {
+    console.log(event.entity);
+    const randomNumber = Math.floor(Math.random() * 9000) + 1000;
     event.entity.tagJoueur = `${randomNumber}`;
     event.entity.fullpseudo = `${event.entity.pseudo}#${event.entity.tagJoueur}`;
     event.entity.amis = [];
+  }
+
+  async conflictAddTag(event: InsertEvent<Joueur> , sameName: Joueur) {
+    let randomNumber = Math.floor(Math.random() * 9000) + 1000;
+      if (sameName) {
+        while (sameName.tagJoueur === `${randomNumber}`) {
+          randomNumber = Math.floor(Math.random() * 9000) + 1000;
+        }
+      }
+      event.entity.tagJoueur = `${randomNumber}`;
+      event.entity.fullpseudo = `${event.entity.pseudo}#${event.entity.tagJoueur}`;
+      event.entity.amis = [];
+  }
+
+
+  async beforeInsert(event: InsertEvent<Joueur>) {
+    try {
+      const sameName = await this.joueurService.findJoueurByPseudo({
+        pseudo: event.entity.pseudo,
+      });
+      await this.conflictAddTag(event, sameName);
+    } catch (error: unknown) {
+      if (error instanceof PlayerNotFound) {
+        await this.addTag(event);
+      } else 
+        throw error;
+    }
   }
 }
--- apps/backend/src/joueurs/joueurs.service.ts
+++ apps/backend/src/joueurs/joueurs.service.ts
@@ -1,4 +1,3 @@
-
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { create } from 'domain';
@@ -21,75 +20,88 @@ export class JoueursService {
     private readonly joueursRepository: Repository<Joueur>,
   ) {}
 
-async inscriptionJoueur(joueurDto: JoueurDto): Promise<Joueur> {
-  try {
-    const newJoueur = this.joueursRepository.create(joueurDto);
-    return await this.joueursRepository.save(newJoueur);
-  } catch (error) {
-    console.log(error.name);
-    if (error.name === 'QueryFailedError') {
-      throw new PlayerAlreadyExists();
+  async inscriptionJoueur(joueurDto: JoueurDto): Promise<Joueur> {
+    try {
+      const newJoueur = this.joueursRepository.create(joueurDto);
+      return await this.joueursRepository.save(newJoueur);
+    } catch (error) {
+      if (error.name === 'QueryFailedError') {
+        throw new PlayerAlreadyExists();
+      }
+      throw new PlayerNotCreated();
+    }
+  }
+
+  async findJoueurByEmail(
+    email: Pick<JoueurDto, 'adresseMail'>,
+  ): Promise<Joueur> {
+    const joueurTrouve = await this.joueursRepository.findOne({
+      where: {
+        adresseMail: email.adresseMail,
+      },
+    });
+    if (!joueurTrouve) {
+      throw new PlayerNotFound();
+    }
+    return joueurTrouve;
+  }
+
+  async findJoueurByFullPseudo(
+    query: Pick<Joueur, 'fullpseudo'>,
+  ): Promise<Joueur> {
+    const joueurTrouve = await this.joueursRepository.findOne({
+      where: {
+        fullpseudo: query.fullpseudo,
+      },
+    });
+    if (!joueurTrouve) {
+      throw new PlayerNotFound();
+    }
+    return joueurTrouve;
+  }
+
+  async findJoueurByPseudo(query: Pick<Joueur, 'pseudo'>): Promise<Joueur> {
+    const joueurTrouve = await this.joueursRepository.findOne({
+      where: {
+        pseudo: query.pseudo,
+      },
+    });
+    if (!joueurTrouve) {
+      throw new PlayerNotFound();
+    }
+    return joueurTrouve;
+  }
+
+  async addFriend(email: string, fullpseudo: string) {
+    const joueur = await this.findJoueurByEmail({ adresseMail: email });
+    const maybeFriend = await this.findJoueurByFullPseudo({
+      fullpseudo: fullpseudo,
+    });
+
+    console.log(joueur.amis);
+
+    if (!joueur.amis) {
+      joueur.amis = [];
     }
 
-    throw new PlayerNotCreated();
+    if (!maybeFriend.amis) {
+      maybeFriend.amis = [];
+    }
+
+    if (joueur.amis.includes(maybeFriend)) throw new AlreadyFriends();
+
+    console.log(joueur.amis);
+
+    joueur.amis.push(maybeFriend);
+    maybeFriend.amis.push(joueur);
+    try {
+      await this.joueursRepository.save(joueur);
+      await this.joueursRepository.save(maybeFriend);
+
+      console.log('Friend added');
+    } catch (error) {
+      console.log(error);
+      throw new Error('Could not add friend');
+    }
   }
-}
-
-async findJoueurByEmail(email: Pick<JoueurDto, 'adresseMail'>): Promise<Joueur> {
-  const joueurTrouve = await this.joueursRepository.findOne({
-    where: {
-      adresseMail: email.adresseMail,
-    },
-  });
-  if (!joueurTrouve) {
-    throw new PlayerNotFound();
-  }
-  return joueurTrouve;
-}
-
-
-async findJoueurByPseudo(query: Pick<Joueur, 'fullpseudo'>): Promise<Joueur> {
-  const joueurTrouve = await this.joueursRepository.findOne({
-    where: {
-      fullpseudo: query.fullpseudo,
-    },
-  });
-  if (!joueurTrouve) {
-    throw new PlayerNotFound();
-  }
-  return joueurTrouve;
-}
-
-async addFriend(email: string, fullpseudo: string) {
-  const joueur = await this.findJoueurByEmail({ adresseMail: email });
-  const maybeFriend = await this.findJoueurByPseudo({
-    fullpseudo: fullpseudo,
-  });
-
-  console.log(joueur.amis);
-
-  if (!joueur.amis) {
-    joueur.amis = [];
-  }
-
-  if (!maybeFriend.amis) {
-    maybeFriend.amis = [];
-  }
-
-  if (joueur.amis.includes(maybeFriend)) throw new AlreadyFriends();
-
-  console.log(joueur.amis);
-
-  joueur.amis.push(maybeFriend);
-  maybeFriend.amis.push(joueur);
-  try {
-    await this.joueursRepository.save(joueur);
-    await this.joueursRepository.save(maybeFriend);
-
-    console.log('Friend added');
-  } catch (error) {
-    console.log(error);
-    throw new Error('Could not add friend');
-  }
-}
 }
